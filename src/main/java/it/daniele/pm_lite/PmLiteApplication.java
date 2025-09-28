d5b3bb7f0310b8a5ed62ba9b8f9f2e2cdbb4cf54
daniele <daniele@ferroli.pw>
2025-09-28 18:44:57 +0200

    feat: implement two endpoints for creating and ingesting frames

---
+++ src/main/java/it/daniele/pm_lite/PmLiteApplication.java
@@ -0,0 +1,13 @@
+package it.daniele.pm_lite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PmLiteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PmLiteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/it/daniele/pm_lite/PmLiteApplicationTests.java
@@ -0,0 +1,13 @@
+package it.daniele.pm_lite;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PmLiteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
